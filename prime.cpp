09d8ab64d7ae5aca74b3db58a45c6045e3f5f42b
ltlexi140 <ltlexi1408@proton.me>
2024-08-09 22:16:42 +1000

    base code for main function

--- prime.cpp
+++ prime.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+
+bool isprime(int n);
 
 int main(){
 
+    std::vector<int> primes;
+
+    for(int i = 0; i < 1000; i++){
+        if(isprime(i)){
+            primes.push_back(i);
+        }
+    }
+
+    for(int i : primes){
+        std::cout << i << ", ";
+    }
     return 0;
+}
+
+bool isprime(int n){
+
+
 }
